3cd56097a27bb4131e525ff3e1d1cf5862def986
Johan Wedfelt <wedfelt@kth.se>
2016-10-24 13:02:30 +0200

    Minor refactoring with method names

--- free_videoplayer_components/free_video_player/free.video.player.js
+++ free_videoplayer_components/free_video_player/free.video.player.js
@@ -1,6 +1,7 @@
 /**
  * @name FREE VIDEO PLAYER
  * @author Johan Wedfelt
+ * @authorUrl https://github.com/DaWoody
  * @license GPLv3, see  {@link http://www.gnu.org/licenses/gpl-3.0.en.html| http://www.gnu.org/licenses/gpl-3.0.en.html}
  * @description A cool open source html5 video library to use when want to play both regular HTML5 content such as mp4, webm but also for adaptive streaming formats such as DASH for instance, Requires the xml2json library to work. Check out more @ {@link http://www.freevideoplayer.org| FreeVideoPlayer.org}
  * @param {object} initiationObject - The initiation object containing information about how to configure the Free Video Player
